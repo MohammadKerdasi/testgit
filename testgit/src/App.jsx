445689e234a9f234fb0abb8ec3d14126f906e620
MohammadKerdasi <mohammadkerdasi003@gmail.com>
2024-06-09 17:10:56 +0200

    test

--- testgit/src/App.jsx
+++ testgit/src/App.jsx
@@ -28,8 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <h1>mohammad
-      </h1>
+      <h2>mohammad</h2>
     </>
   )
 }
